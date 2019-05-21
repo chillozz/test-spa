2a623a5a90fe28b01cdc5da9896c0e966c0bd86f
Artem Terehin <aterehin@accesssoftek.com>
2019-05-22 00:59:37 +0500

    Code refactoring

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,62 +1,15 @@
 import 'normalize.css/normalize.css';
 import '../styles/index.scss';
 
-const apiUrl = 'https://apilayer.net/api/check';
-const apiKey = 'ce6809af39e8e5d2516ed3d80010a84e';
+import Validator from './validator.js';
 
 const form = document.querySelector('#email-form');
-const input = form.querySelector('input[name=email]');
-const button = form.querySelector('input[type=submit]');
-const notifier = form.querySelector('.notifier');
-
-const messages = {
-	invalid: 'Invalid format',
-	valid: 'Valid format'
-}
-
-form.addEventListener('submit', e => {
-	e.preventDefault();
-
-	setButtonstate(false);
-
-	fetch(apiUrl+`?access_key=${apiKey}&email=${input.value}`)
-	.then(response => response.json())
-	.then(data => {
-		const message = data.format_valid ? messages.valid : messages.invalid;
-		const type = data.format_valid ? 'success' : 'error';
-
-		setButtonstate(true);
-		notify(message, type);		
-	});
-});
-
-input.addEventListener('keyup', e => {
-	if(e.keyCode !== 13) {
-		setButtonstate(input.value !== '');
-	}
-});
+const validator = new Validator('ce6809af39e8e5d2516ed3d80010a84e', form);
 
 
-function notify(msg, type) {
-	let className;
 
-	switch(type) {
-		case 'error':
-			className = 'notifier_state_error';
-		break;
-		case 'success':
-			className = 'notifier_state_success';
-		break;
-	}
 
-	notifier.innerHTML = msg;
-	notifier.className = `notifier ${className}`;
-}
 
-function setButtonstate(enabled) {
-	if(enabled) {
-		button.removeAttribute("disabled");
-	} else {
-		button.setAttribute("disabled", "disabled");
-	}
-}
+
+
+
--- src/scripts/validator.js
+++ src/scripts/validator.js
@@ -1,5 +1,67 @@
-class Validator {
-	constructor(apikey) {
+export default class Validator {
+	constructor(apiKey, form) {
+		this.apiKey = apiKey;
+		this.form = form;
 
+		this.apiUrl = 'https://apilayer.net/api/check';
+		this.input = this.form.querySelector('input[name=email]');
+		this.button = this.form.querySelector('input[type=submit]');
+		this.notifier = this.form.querySelector('.notifier');
+		this.messages = {
+			invalid: 'Invalid format',
+			valid: 'Valid format'
+		};
+
+		this.init();
 	} 
+
+	init() {
+		this.form.addEventListener('submit', e => {
+			e.preventDefault();
+
+			this.setButtonstate(false);
+			this.fetchData().then(() => { this.setButtonstate(true); });
+		});
+
+		this.form.addEventListener('keyup', e => {
+			if(e.keyCode !== 13) {
+				this.setButtonstate(this.input.value !== '');
+			}
+		});
+	}
+
+	fetchData() {
+		return fetch(this.apiUrl+`?access_key=${this.apiKey}&email=${this.input.value}`)
+			.then(response => response.json())
+			.then(data => {
+				const message = data.format_valid ? this.messages.valid : this.messages.invalid;
+				const type = data.format_valid ? 'success' : 'error';
+
+				this.notify(message, type);		
+			});
+	}
+
+	notify(msg, type) {
+		let className;
+
+		switch(type) {
+			case 'error':
+				className = 'notifier_state_error';
+			break;
+			case 'success':
+				className = 'notifier_state_success';
+			break;
+		}
+
+		this.notifier.innerHTML = msg;
+		this.notifier.className = `notifier ${className}`;
+	}
+
+	setButtonstate(enabled) {
+		if(enabled) {
+			this.button.removeAttribute("disabled");
+		} else {
+			this.button.setAttribute("disabled", "disabled");
+		}
+	}
 }
